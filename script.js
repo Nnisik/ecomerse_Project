0c8efc61c270be765ca6528cc7d2bd5fab49d553
Nnisik <nisik0120@gmail.com>
2023-04-03 20:28:36 +0300

    finished login page

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+function hideAllPages() {
+    document.querySelector('.signup-page').style.display = 'none';
+    document.querySelector('.login-page').style.display = 'none';
+}
+
+function showPage(page) {
+    hideAllPages()
+    document.querySelector(`#${page}`).style.display = 'block';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+    // display changing
+    document.querySelector("#nav-links-logout").style.display = 'none';
+    document.querySelector('.signup-page').style.display = 'none';
+
     // go to sign in page button
     document.querySelector('#nav-links-signin').onclick = () => {
         window.location.href = "signin.html";
@@ -8,4 +22,10 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#nav-links-cart').onclick = () => {
         window.location.href = "shop-cart.html";
     };
+
+    document.querySelectorAll('.page').forEach(button => {
+        button.onclick = function() {
+            showPage(this.dataset.page);
+        }
+    });
 });
