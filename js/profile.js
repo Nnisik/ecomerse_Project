7cc89d79dd868ce9b7e92868ed376d4d1e8c942b
Nnisik <nisik0120@gmail.com>
2023-04-19 20:19:30 +0300

    finished js for profile page

--- js/profile.js
+++ js/profile.js
@@ -1,48 +1,47 @@
-function addNewProduct() {
-    let new_prod = document.createElement("div");
+document.addEventListener("DOMContentLoaded", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  // TODO: get user information from db (node or connect to php or json)
+
+  document.querySelector("#nav-links-back").onclick = function () {
+    let userId = urlParams.get("id");
+    location.href = `index.html?id=${userId}`;
+  };
+
+  document.querySelector("#new-create").onclick = function () {
+    const name = document.querySelector("#goods-new-name").value;
+    const categ = document.querySelector("#goods-new-cat").value;
+    const price = document.querySelector("#goods-new-price").value;
+    const desc = document.querySelector("#goods-new-desc").value;
+
+    const new_prod = document.createElement("div");
     new_prod.className = "good";
 
-    // adding image
-    new_prod.append(document.createElement("img"));
-    
-    // adding inner div
-    let new_inner_div = document.createElement("div");
-    
-    // product header
-    let prod_head = document.querySelector("h3");
-    prod_head.innerHTML = document.querySelector("#goods-new-name").value;
-    new_inner_div.append(prod_head);
-    
-    // product price
-    let prod_price = document.querySelector("p");
-    prod_price.innerHTML = document.querySelector("#goods-new-price").value;
-    new_inner_div.append(prod_price);
-    
-    new_prod.append(new_inner_div);
+    const new_prod_info = document.createElement("div");
+    new_prod_info.className = "good-info";
 
-    // product description
-    let prod_desc = document.querySelector("p");
-    prod_desc.innerHTML = document.querySelector("#goods-new-desc").value;
-    new_prod.append(document.createElement(prod_desc));
+    const new_name = document.createElement("h5");
+    new_name.innerHTML = name;
+    new_prod_info.append(new_name);
 
+    const new_categ = document.createElement("h6");
+    new_categ.innerHTML = categ;
+    new_prod_info.append(new_categ);
+
+    const new_desc = document.createElement("p");
+    new_desc.innerHTML = desc;
+    new_prod_info.append(new_desc);
+
+    const new_price = document.createElement("h4");
+    new_price.innerHTML = price;
+    new_prod_info.append(new_price);
+
+    new_prod.append(new_prod_info);
     document.querySelector("#goods").append(new_prod);
-}
 
-document.addEventListener("DOMContentLoaded", function() {
-    const urlParams = new URLSearchParams(window.location.search);
-
-    // TODO get user information from db (node or connect to php or json)
-
-    // TODO create new goods
-    document.querySelector("#new-create").onclick = function() {
-        addNewProduct();
-        document.querySelector("#goods-new-name").value = "";
-        document.querySelector("#goods-new-price").value = "";
-        document.querySelector("#goods-new-desc").value = "";
-    };
-
-    document.querySelector("#nav-links-back").onclick = function() {
-        let userId = urlParams.get("id");
-        location.href = `index.html?id=${userId}`;
-    };
+    document.querySelector("#goods-new-name").value = "";
+    document.querySelector("#goods-new-cat").value = "";
+    document.querySelector("#goods-new-price").value = "";
+    document.querySelector("#goods-new-desc").value = "";
+  };
 });
